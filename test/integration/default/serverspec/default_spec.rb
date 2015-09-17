3ed8395d513129d701237e4d34714e4aca7e371f
Tim Smith <tsmith84@gmail.com>
2015-09-17 11:55:26 -0700

    Use the new serverspec format

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,16 +1,15 @@
 require 'serverspec'
 
-include Serverspec::Helper::Exec
-include Serverspec::Helper::DetectOS
+set :backend, :exec
 
 describe 'awscreds::default' do
   describe file('/root/.aws') do
     it { should be_directory }
   end
   describe file('/root/.aws/config') do
-    its(:content) {
+    its(:content) do
       should eq File.read(
         File.expand_path('../test-output.txt', __FILE__))
-    }
+    end
   end
 end

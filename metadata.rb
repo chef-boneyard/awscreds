550c5b3e5bc7dce5137960bc775c065ccca58acf
Tim Smith <tsmith84@gmail.com>
2019-10-30 14:09:50 -0700

    Cookstyle fixes
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,10 +3,6 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache-2.0'
 description 'Creates /root/.aws/config from chef-vault'
-long_description %(
-  Loads credentials from chef vault and creates an aws config file (.ini
-  format) from the loaded credentials. Supports multiple profiles and
-  alternate locations for the credentials file.)
 version '1.0.2'
 
 %w(amazon centos debian mac_os_x opensuse opensuseleap oracle redhat scientific solaris2 suse ubuntu windows aix).each do |os|

a2e6aebc95daa13c48dbb3c42ed6a7c0a496e2c4
Tim Smith <tsmith84@gmail.com>
2020-01-05 11:58:23 -0800

    Require Chef 12.15+
    This gives us modern metadata parsing
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -13,4 +13,4 @@ depends 'chef-vault'
 
 source_url 'https://github.com/chef-cookbooks/awscreds'
 issues_url 'https://github.com/chef-cookbooks/awscreds/issues'
-chef_version '>= 12.1' if respond_to?(:chef_version)
+chef_version '>= 12.15'
